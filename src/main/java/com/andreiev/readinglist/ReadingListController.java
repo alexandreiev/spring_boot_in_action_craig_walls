a259d3520965e21fed86f419c0c2e1b82870dab5
Oleksand Andreiev <andreyew89gmail.com>
2021-10-03 14:23:56 +0300

    custom datasource config and AmazonID property have been added

--- src/main/java/com/andreiev/readinglist/ReadingListController.java
+++ src/main/java/com/andreiev/readinglist/ReadingListController.java
@@ -1,14 +1,21 @@
 package com.andreiev.readinglist;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
+@ConfigurationProperties(prefix="amazon")  // inject with properties
 public class ReadingListController {
 
+    @Getter
+    @Setter
+    private String associateId;
     private ReadingListRepository readingListRepository;
 
     @Autowired
@@ -19,8 +26,11 @@ public class ReadingListController {
     @GetMapping(value = "/{reader}")
     public String readersBooks(@PathVariable("reader") String reader, Model model) {
         var readingList = readingListRepository.findByReader(reader);
-        if (readingList != null)
+        if (readingList != null) {
             model.addAttribute("books", readingList);
+            model.addAttribute("reader", reader);
+            model.addAttribute("amazonID", associateId);
+        }
         return "readingList";
     }
 
@@ -28,6 +38,6 @@ public class ReadingListController {
     public String addToReadingList(@PathVariable("reader") String reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
-        return "redirect:/{reader}";
+        return "redirect:/";
     }
 }
