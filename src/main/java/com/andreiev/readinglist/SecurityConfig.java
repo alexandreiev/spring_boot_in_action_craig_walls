e699878e1f4b23d431a1f8a6adbb98e94e599929
Oleksand Andreiev <andreyew89gmail.com>
2021-10-07 21:49:53 +0300

    Playing with profiles

--- src/main/java/com/andreiev/readinglist/SecurityConfig.java
+++ src/main/java/com/andreiev/readinglist/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.andreiev.readinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+@Profile("production")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
