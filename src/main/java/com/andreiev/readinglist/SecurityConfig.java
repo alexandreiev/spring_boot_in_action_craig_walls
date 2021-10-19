e480a8eefbdd1e6b0bd0b3da3c7182ba2549076d
Oleksandr Andreiev <oleksandr.andreiev@spacelabs.com>
2021-10-19 10:50:58 +0300

    minor changes and fixes in security

--- src/main/java/com/andreiev/readinglist/SecurityConfig.java
+++ src/main/java/com/andreiev/readinglist/SecurityConfig.java
@@ -2,16 +2,12 @@ package com.andreiev.readinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-@Profile("production")
+//@Profile("production")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
